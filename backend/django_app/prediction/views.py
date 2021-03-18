9f22dd9253e49ef73dc19d4f303748cbfc5d973c
WillYip716 <william.yip.716@gmail.com>
2021-03-17 19:45:18 -0700

    test http response

--- backend/django_app/prediction/views.py
+++ backend/django_app/prediction/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import pickle
+import pandas as pd
+import numpy as np
+import json
+from django.http import HttpResponse
 
-# Create your views here.
+with open(f'model/visitor.pkl', 'rb') as f:
+    vmodel = pickle.load(f)
+
+with open(f'model/home.pkl', 'rb') as f:
+    hmodel = pickle.load(f)
+
+
+def predict(request):
+    data = {
+        'name': 'Vitor',
+        'location': 'Finland',
+        'is_active': True,
+        'count': 28
+    }
+    dump = json.dumps(data)
+
+    return HttpResponse(dump, content_type='application/json')
