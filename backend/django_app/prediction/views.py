8604d7c9169a61f4a968c6f6c9fc49cb068d4d23
WillYip716 <william.yip.716@gmail.com>
2021-03-19 18:04:14 -0700

    json setup for predictions

--- backend/django_app/prediction/views.py
+++ backend/django_app/prediction/views.py
@@ -12,12 +12,40 @@ with open(f'model/home.pkl', 'rb') as f:
 
 
 def predict(request):
-    data = {
-        'name': 'Vitor',
-        'location': 'Finland',
-        'is_active': True,
-        'count': 28
-    }
-    dump = json.dumps(data)
+
+    data = pd.read_csv('model/merged_mldata2020v2.csv',delimiter = ',')
+    outputs = []
+    games = data.drop(columns =['vscore', 'hscore','winner','ou',"sWinner","fspread","fscore","vospread","vcspread"])
+    games = pd.DataFrame(games)
+    
+    weekfilter = games['week'] == 8
+
+    games = games[weekfilter]
+
+    #data.corr()
+    columnheads = ['temp','wind_mph','vdflg','hdflg','divgame','nsite','hospread','ouopen','hcspread','ouclose','vTOTAL.DVOA','vTOTAL.RNK','vOFF.RNK','vOFF.DVOA','vDEF.RNK','vDEF.DVOA','vST.RNK','vST.DVOA','hTOTAL.DVOA','hTOTAL.RNK','hOFF.RNK','hOFF.DVOA','hDEF.RNK','hDEF.DVOA','hST.RNK','hST.DVOA','vtsw','vtsl','vtst','htsw','htsl','htst','vtw','vtl','vtt','vts','htw','htl','htt','hts']
+
+    games = np.hsplit(games,[3])
+    tw = games[0]
+    pdata = games[1]
+    input_variables = pd.DataFrame(pdata, columns = columnheads,dtype=float)
+
+    
+    #input_variables["visitor_pred"] = vmodel.predict(input_variables)
+
+
+    visitorpred = vmodel.predict(input_variables)
+    homepred = hmodel.predict(input_variables)
+
+    input_variables["visitor_pred"] = visitorpred
+    input_variables["home_pred"] = homepred
+
+    output_info = input_variables[["hcspread","ouclose","visitor_pred","home_pred"]]
+    output_info = tw.join(output_info)
+
+    result = output_info.to_json(orient="table")
+    parsed = json.loads(result)
+
+    dump = json.dumps(parsed["data"])
 
     return HttpResponse(dump, content_type='application/json')
